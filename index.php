2d8d720e28ff1cf535ab3add88ae549284195ddb
CorLabs <pivovarov.id@yandex.ru>
2024-07-15 11:17:10 +0300

    Index Page

---
+++ index.php
@@ -0,0 +1,105 @@
+ <!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home</title>
+<meta name="yandex-verification" content="97152aba9ed18c2e" />
+<script src="https://unpkg.com/@tonconnect/ui@latest/dist/tonconnect-ui.min.js"></script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style>
+<link rel="icon" type="image/x-icon" href="Untitled.png">
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body style = "font-family: Ubuntu, Helvetica, serif; background-color:  #D9D9D9; text-align: center; color:#313131">
+	<div>
+	<br>
+	<h1 style = "font-size: 435%">Cor<strong style = "color:red;">Labs</strong></h1>
+	<br>
+	</div>
+	<hr>
+	<br>
+	<div>
+		<h3 style = "padding-right: 55%;font-family: Kanit">About</h3>
+       <span class="material-symbols-outlined">
+feature_search
+</span> <a href="about.html" class="w3-button w3-hover w3-large w3-black" style="width: 55%; height: auto; border-width:medium;" target="_parent">  <span class="material-symbols-outlined">info</span> About</a>
+		
+        
+    </div>
+	<hr>
+	<div>
+		<h3 style = "padding-right: 55%;font-family: Kanit">Products</h3>
+       <span class="material-symbols-outlined">
+feature_search
+</span> <a href="prds.html" class="w3-button w3-hover-cyan w3-large w3-black" style="width: 55%; height: auto; border-width:medium;" target="_parent">  <span class="material-symbols-outlined">
+event_list
+</span> Product List </a>
+		<br>
+	<span class="material-symbols-outlined">
+flowsheet
+</span>	<a href = "dnlds.html" class = "w3-button w3-hover-cyan w3-large w3-black" style="width: 55%; height: auto; border-width: medium" target = "_parent"> <span class="material-symbols-outlined">
+deployed_code_update
+</span>  Downloads  </a>
+        
+    </div>
+	<hr>
+	<div>
+		<h3 style = "padding-right: 55%;font-family: Kanit">Documents</h3>
+       <span class="material-symbols-outlined">
+description
+</span> <a href="docs.html" class="w3-button w3-hover-red w3-large w3-black" style="width: 55%; height: auto; border-width:medium;" target="_parent">  <span class="material-symbols-outlined">
+integration_instructions
+		</span> Documentations</a>
+		<br>
+	<span class="material-symbols-outlined">
+description
+</span>	<a href = "prtx.html" class = "w3-button w3-hover-red w3-large w3-black" style="width: 55%; height: auto; border-width: medium" target = "_parent"> <span class="material-symbols-outlined">
+co_present
+</span> Product release presentations </a>
+        
+    </div>
+	<hr>
+	<div id="donation">
+		<h3 style = "padding-right: 55%;font-family: Kanit">Donate</h3>
+		<h4 style = "padding-right: 55%;font-family: Kanit">In cryptocurrency</h4>
+		<div>
+		
+		<div>
+	</div>
+	<hr>
+	<footer>
+		<h6>CorLabs; 2024</h6>
+		<p>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+    </a>
+</p>
+		<p>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+    </a>
+</p>
+	</footer>
+</body>
+</html>
